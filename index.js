2e2f6e23cd1fa68d65667cf498bade6a5d37f652
kantegory <davidobryakov@gmail.com>
2020-08-14 19:40:30 +0300

    Implement /saveToken method

--- index.js
+++ index.js
@@ -12,8 +12,23 @@ const app = express();
 const server = require('http').createServer(app);
 const bodyParser = require('body-parser');
 
+// PG functions
+// -- notifications
+const saveNotification = require('./utils/pg/notifications/saveNotification');
+const getNotifications = require('./utils/pg/notifications/getNotifications');
+// -- topics
+const saveTopic = require('./utils/pg/topics/saveTopic');
+const getTopics = require('./utils/pg/topics/getTopics');
+// -- user devices
+const saveUserDevice = require('./utils/pg/userDevices/saveUserDevice');
+const getUserDevices = require('./utils/pg/userDevices/getUserDevices');
+// -- subscriptions
+const saveSubscriptions = require('./utils/pg/subscriptions/saveSubscriptions');
+const getSubscriptions = require('./utils/pg/subscriptions/getSubscriptions');
+
 // FCM functions
 const sendPush = require('./utils/fcm/send');
+const subscribe = require('./utils/fcm/subscribe');
 
 // app params
 const port = __config.port;
@@ -29,21 +44,32 @@ app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/client/index.html`);
 })
 
+// -- REST API endpoints
+// -- -- save token endpoint
 app.post('/saveToken', (req, res) => {
+  // get body from request
   let body = req.body;
   
+  // get data from body
   let token = body.token;
   let userId = body.userId;
 
   console.log(`NEW TOKEN FOR USER ID ${userId} IS ${token}`);
 
+  // save user device to db
+  let values = [userId, token];
+  saveUserDevice(values);
+  
+  // send response
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write('{"success": true}');
   res.end();
 })
 
+// -- -- send push endpoint
 app.post('/send', (req, res) => {
   let body = req.body;
+
   let payload = body.payload;
   let tokens = body.tokens;
 
@@ -53,3 +79,22 @@ app.post('/send', (req, res) => {
   res.write('{"success": true}');
   res.end();
 })
+
+// -- -- topic endpoint
+app.post('/topic', (req, res) => {
+  let body = req.body;
+
+  let title = body.title;
+  let description = body.description;
+})
+
+// -- -- subsription endpoint
+app.post('/subscribe', (req, res) => {
+  let body = req.body;
+
+  let userId = body.userId;
+  let topicId = body.topicId;
+
+  // let userTokens = 
+})
+
--- utils/pg/userDevices/saveUserDevice.js
+++ utils/pg/userDevices/saveUserDevice.js
@@ -1,10 +1,37 @@
 const client = require('../connector')
 
-const saveUserDevice = (values) => {
+// dependecies
+const getUserDevices = require('./getUserDevices');
 
-  const query = {
+const saveUserDevice = async (values) => {
+
+  let userId = Number(values[0]);
+  let deviceToken = values[1];
+
+  let query = {
     text: 'INSERT INTO user_devices (user_id, tokens) VALUES ($1, $2);',
-    values: [Number(values[0]), JSON.stringify(values[1])],
+    values: [userId, JSON.stringify([deviceToken])],
+  }
+
+  // get all of user previous device tokens
+  let userDevices = await getUserDevices(userId);
+
+  // check if not empty, then update devices
+  if (userDevices.length) {
+    console.log("devices are", userDevices);
+    let tokens = userDevices[0].tokens;
+
+    // check if tokens not includes current token, then add
+    if (!tokens.includes(deviceToken)) {
+      tokens.push(deviceToken);
+    }
+
+    tokens = JSON.stringify(tokens);
+
+    query = {
+      text: 'UPDATE user_devices SET tokens = $2 WHERE user_id = $1;',
+      values: [userId, tokens],
+    }
   }
 
   client.query(query, (err, res) => {
