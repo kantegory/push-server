f531fd0040843c55ca5c48acade831861c306f82
kantegory <davidobryakov@gmail.com>
2020-08-14 16:18:40 +0300

    Remove socket.io

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const __config = configParser(__configPath, __moduleName);
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
 const bodyParser = require('body-parser');
 
 // FCM functions
@@ -51,23 +50,4 @@ app.post('/send', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write('{"success": true}');
   res.end();
-})
-
-// socket.io logic
-let connections = [];
-
-io.sockets.on('connection', (socket) => {
-  console.log('SUCCESS CONNECTION');
-  connections.push(socket);
-
-  socket.on('disconnect', (data) => {
-    connections.splice(connections.indexOf(socket), 1);
-    console.log('DISCONNECT');
-  })
-
-  socket.on('sendPush', (data) => {
-    // send push to users in online
-    io.sockets.emit('showPush', { text: data.text });
-    console.log('PUSH SENDED SUCCEFULLY');
-  })
 })
